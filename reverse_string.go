77365f86bbaf662816f716d32ccb291c2b7f543f
iNaz07 <ekrezan.94@mail.ru>
2022-05-14 19:04:53 +0600

    reversed

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,15 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	runes := []rune(input)
+	for i := len(runes) - 1; i >= 0; i-- {
+		output += string(runes[i])
+	}
 	return output
 }
+
+// func main() {
+// 	fmt.Println(ReverseString("Hello world!"))
+// 	fmt.Println(ReverseString("Hello\n world!"))
+// 	fmt.Println(ReverseString("Señor"))
+// }
